94ac8a0ed1329eefd289a35ca49aa5c7f732d84d
Markus Sahrakorpi <markus.sahrakorpi@gmail.com>
2018-10-19 20:03:38 +0300

    delete thihs shit

---
+++ robot.py
@@ -0,0 +1,10 @@
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+
+sound = Sound()
+sound.speak('Welcome to the E V 3 dev project!')
+
+print("Hello World")
