a43ecc463cefd232cff428240d3bb7d0e20a54de
Richard Gunning <richard.gunning@frantic.com>
2018-10-19 20:31:25 +0300

    initial commit

--- robot.py
+++ robot.py
@@ -1,10 +1,28 @@
+#!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
-from ev3dev2.sound import Sound
 
-sound = Sound()
-sound.speak('Welcome to the E V 3 dev project!')
+# TODO: Add code here
+print("hello world")
 
-print("Hello World")
+# drive in a turn for 5 rotations of the outer motor
+# the first two parameters can be unit classes or percentages.
+#tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(75), 10)
+
+# drive in a different turn for 3 seconds
+#tank_drive.on_for_seconds(SpeedPercent(60), SpeedPercent(30), 3)
+
+def drive_forward(time=10, speed=50, direction=0):
+  tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+  left_speed = min(max(speed * (1-direction),100),1)
+  right_speed = min(max(speed * (1+direction),100),1)
+
+  tank_drive.on_for_rotations(SpeedPercent(left_speed), SpeedPercent(right_speed), 10)
+
+drive_forward(10,100,0)
+drive_forward(10,-50,0)
+drive_forward(10,50,2)
+drive_forward(10,50,-2)
+
